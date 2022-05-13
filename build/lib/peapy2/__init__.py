e1d383d248ea95646cb7ba2962f46ceba10adf5e
Genius1512 <silvan.genius@gmail.com>
2022-05-13 10:54:45 +0200

    Removed build stuff

--- build/lib/peapy2/__init__.py
+++
@@ -1,6 +0,0 @@
-from .component import Component
-from peapy2 import exceptions
-from .game_object import GameObject
-from .peapy import PeaPy
-from .transform import Transform
-from .window import Window
--- build/lib/peapy2/__pygame.py
+++
@@ -1,7 +0,0 @@
-"""
-Import pygame without printing the welcome message.
-"""
-import os
-
-os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
-import pygame
